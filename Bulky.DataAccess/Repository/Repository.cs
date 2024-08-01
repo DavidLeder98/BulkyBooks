5472ae87603feb81dfc1da86c395cc3985a01326
David Leder <davidleder98@gmail.com>
2024-08-01 17:34:19 +0200

    added repository interface

---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+	public class Repository<T> : IRepository<T> where T : class
+	{
+		private readonly ApplicationDbContext _db;
+		internal DbSet<T> dbSet;
+		public Repository(ApplicationDbContext db)
+		{
+			_db = db;
+			this.dbSet = _db.Set<T>();
+			// it's the same as _db.Categories == dbSet
+		}
+
+		public void Add(T entity)
+		{
+			dbSet.Add(entity);
+		}
+
+		public T Get(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<T> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveRange(IEnumerable<T> entities)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
